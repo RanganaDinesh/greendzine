49053aad891c714486241c98a6ccae4d3005fa84
karthik1131 <rangana.dinesh09@gmail.com>
2023-06-23 12:24:22 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Component from './Component'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users?page=2')
+      .then((response) => response.json())
+      .then((data) => setUsers(data.data));
+  }, []);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const filteredUsers = users.filter((user) =>
+      user.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredUsers);
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className='container-bar'>
+          <form onSubmit={handleSearch}>
+        <input
+          className='searchbar'
+          type="text"
+          placeholder="Type first name"
+          value={searchTerm}
+          onChange={handleChange}
+          
+        /> 
+        <button type="submit">Search</button>
+      </form>
+     </div>
+
+      <div className='container-avatar'>
+      {searchResults.map((user) => (
+        <div key={user.id}  >
+          <span className='id-pop'>{user.id}</span>
+                <div className='container'>
+                    
+                    <img src={user.avatar} alt="avatar image"  />
+                </div>
+                  <h4>{user.first_name}</h4>
+              </div>
+          ))}
+      </div>
+      
+    </div>
+  )
+}
+
+export default UserList
